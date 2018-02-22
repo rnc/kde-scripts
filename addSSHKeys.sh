fec2518da9cd492386c50d57657a6c53235785fc
Nick Cross <ncross@redhat.com>
2018-02-22 16:10:40 +0000

    Try to avoid password requests within script

--- addSSHKeys.sh
+++ addSSHKeys.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Short script to use ksshaskpass (http://kde-apps.org/content/show.php?content=50971)
 # to add all default SSH identities
@@ -11,5 +11,5 @@ ssh-add
 # Add all others that I can find.
 for i in `find $HOME/.ssh -name "id*" | grep -v "\.pub"`
 do
-    ssh-add $i
+    ssh-add $i < /dev/null
 done
