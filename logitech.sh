77a4e468997bfbca964337e96cfd9a165937ced0
Nick Cross <ncross@redhat.com>
2019-06-24 22:37:24 +0100

    Add comment

--- logitech.sh
+++ logitech.sh
@@ -2,7 +2,13 @@
 #
 # This script will reinit the Logitech USB Mouse to my preferred
 # button layout.
-
+#
+#
+# Note: this is broken in KDE Plasma/QT by
+# https://bugreports.qt.io/browse/QTBUG-49345
+# WONTFIX:
+# https://bugs.freedesktop.org/show_bug.cgi?id=96759
+#
 xinput query-state "Logitech USB Laser Mouse" > /dev/null 2>&1
 if [ "$?" == "0" ]
 then
